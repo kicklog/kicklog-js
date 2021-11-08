b72f748153ada0c156f81e28d9c654eee9ecf85b
miki-matsumoto <mikimatsumoto829@gmail.com>
2021-11-08 23:24:31 +0900

    Lint

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,4 @@
-import {Kicklog, KicklogConstructor} from '../types'
+import { Kicklog, KicklogConstructor } from "../types";
 
 export type LoadKicklog = (
   ...args: Parameters<KicklogConstructor>
@@ -9,20 +9,18 @@ export interface LoadParams {
 }
 
 export const initKicklog = (
-  maybe: KicklogConstructor| null,
+  maybe: KicklogConstructor | null,
   args: Parameters<KicklogConstructor>
-): Kicklog| null => {
+): Kicklog | null => {
   if (maybe === null) return null;
 
   const kicklog = maybe.apply(undefined, args);
   return kicklog;
 };
 
-let kicklogPromise: Promise<KicklogConstructor| null> | null = null;
+let kicklogPromise: Promise<KicklogConstructor | null> | null = null;
 
-export const loadScript = (
-  params: null | LoadParams
-): Promise<KicklogConstructor| null> => {
+export const loadScript = (): Promise<KicklogConstructor | null> => {
   if (kicklogPromise !== null) return kicklogPromise;
 
   kicklogPromise = new Promise((resolve, reject) => {
@@ -36,7 +34,6 @@ export const loadScript = (
       return;
     }
 
-    // if nothing script process here...
     try {
       let script = findScript();
       if (!script) script = injectScript();
@@ -58,18 +55,18 @@ export const loadScript = (
   return kicklogPromise;
 };
 
-const V1_URL = "https://kicklog.vercel.app/js/v0";
-const V1_URL_REGEX = /^https:\/\/kicklog\.vercel\.app\/js\/v0\/?(\?.*)?$/;
+const V0_URL = "https://kicklog.co/js/v0";
+const V0_URL_REGEX = /^https:\/\kicklog\.co\/js\/v0\/?(\?.*)?$/;
 
 const findScript = (): HTMLScriptElement | null => {
   const scripts = document.querySelectorAll<HTMLScriptElement>(
-    `script[src^="${V1_URL}"]`
+    `script[src^="${V0_URL}"]`
   );
 
   for (let i = 0; i < scripts.length; i++) {
     const script = scripts[i];
 
-    if (!V1_URL_REGEX.test(script.src)) {
+    if (!V0_URL_REGEX.test(script.src)) {
       continue;
     }
 
@@ -80,7 +77,7 @@ const findScript = (): HTMLScriptElement | null => {
 };
 const injectScript = (): HTMLScriptElement => {
   const script = document.createElement("script");
-  script.src = `${V1_URL}`;
+  script.src = `${V0_URL}`;
 
   const headOrBody = document.head || document.body;
 
