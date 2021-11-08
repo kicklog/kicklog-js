9f87e31017fcaf18ec8ae1dca17090df48f5a3a9
miki-matsumoto <mikimatsumoto829@gmail.com>
2021-11-08 23:25:00 +0900

    Method types

--- types/kicklog-js/index.d.ts
+++ types/kicklog-js/index.d.ts
@@ -1,7 +1,28 @@
+type TrackRequest = {
+  userId: string;
+  customerId: string;
+  action: string;
+};
+
+type CustomerRequest = {
+  customerId: string;
+  name?: string;
+};
+
+type UserRequest = {
+  userId: string;
+  customerId: string;
+  name?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  avatar?: string;
+};
+
 export interface Kicklog {
-  customer(): Promise<any>
-  user(): Promise<any>
-  track(): Promise<any>
+  customer(req: CustomerRequest): Promise<any>;
+  user(req: UserRequest): Promise<any>;
+  track(req: TrackRequest): Promise<any>;
 }
 
 export interface KicklogConstructor {
